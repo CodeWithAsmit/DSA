e12be06b6e8720a7b183693870f5dd49727c6758
Asmit Mohan <mohanasmit@gmail.com>
2025-01-08 00:08:58 +0530

    Update Count Squares.cpp

--- Mathematics/Count Squares.cpp
+++ Mathematics/Count Squares.cpp
@@ -1,3 +1,5 @@
+#Time and Space complexity O(1)
+
 class Solution
 {
   public:
@@ -12,5 +14,43 @@ class Solution
         {
             return temp;
         }
+    }
+};
+
+#Time complexity is log(n) and Space complexity is O(1)
+
+class Solution
+{
+  public:
+    int countSquares(int n)
+    {
+        if (n==1)
+        {
+            return 0;
+        }
+        
+        long low = 1;
+        long high = n;
+        long mid = 0;
+        
+        while (low <= high)
+        {
+            mid = low + ((high - low) / 2);
+            long temp = mid * mid;
+            
+            if (temp == n)
+            {
+                return mid-1;  
+            }
+            else if (temp > n)
+            {
+                high = mid - 1;  
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        return (int)high;
     }
 };
