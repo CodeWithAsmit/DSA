f4c4c59d08d03462a1ead9f8d4af25f94eae8271
Asmit Mohan <mohanasmit@gmail.com>
2025-01-16 22:59:39 +0530

    Update and rename Equilibrium Point.cpp to Find Pivot Index.cpp

--- Array/Find Pivot Index.cpp
+++ Array/Find Pivot Index.cpp
@@ -8,16 +8,19 @@ public:
        int n=nums.size();
        vector<int>prefix(n,0);
        vector<int>suffix(n,0);
+        
        prefix[0]=nums[0]; 
        for(int i=1;i<nums.size();i++)
        {
            prefix[i]=prefix[i-1]+nums[i];
        }
+        
        suffix[n-1]=nums[n-1];
        for(int i=n-2;i>=0;i--)
        {
            suffix[i]=suffix[i+1]+nums[i];
        }
+        
        for(int i=0;i<nums.size();i++)
        {
            if(prefix[i]==suffix[i])
