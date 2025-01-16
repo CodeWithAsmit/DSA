5ed485f427d2e3fcfeaa2d27b72c93bd48ce24a0
Asmit Mohan <mohanasmit@gmail.com>
2025-01-17 00:12:03 +0530

    Update Spiral Matrix 2.cpp

--- Array/Spiral Matrix 2.cpp
+++ Array/Spiral Matrix 2.cpp
@@ -5,13 +5,10 @@ public:
     {
         vector<vector<int>>ans(n,vector<int>(n));
         
-        int row=n;
-        int col=n;
         int top=0;
-        int left=0;
-        
-        int right=col-1;
-        int down=row-1;
+        int left=0;        
+        int right=n-1;
+        int down=n-1;
         
         int count=1;
         
@@ -27,7 +24,6 @@ public:
             {
                  ans[i][right]=count++;
             }
-
             right--;
 
             if(top<=down)
