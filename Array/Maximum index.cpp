69d5fc44966d7d87a317fc8b78547846043846d4
Asmit Mohan <mohanasmit@gmail.com>
2025-01-21 22:00:59 +0530

    Update Maximum index.cpp

--- Array/Maximum index.cpp
+++ Array/Maximum index.cpp
@@ -1,39 +1,43 @@
 class Solution
 {
-    public:
-    int maxIndexDiff(int arr[], int n) 
-    { 
-       int left[n];
-       int right[n];
-       
-       left[0]=arr[0];
-       for(int i=1;i<n;i++)
-       {
-           left[i]=min(left[i-1],arr[i]);
-       }
-       
-       right[n-1]=arr[n-1];
-       for(int i=n-2;i>=0;i--)
-       {
-           right[i]=max(right[i+1],arr[i]);
-       }
-       
-       int i=0;
-       int j=0;
-       int ans=INT_MIN;
-       
-       while(i<n&&j<n)
-       {
-           if(right[j]>=left[i])
-           {
-               ans=max(ans,j-i);
-               j++;
-           }
-           else
-           {
-               i++;
-           }
-       }
-       return ans;
+  public:
+    int maxIndexDiff(vector<int>& arr)
+    {
+        int n=arr.size();
+        int i=0;
+        int j=n-1;
+        int ans=INT_MIN;
+        
+        vector<int>leftToRightMin(n,0);
+        vector<int>RightToLeftMax(n,0);
+        
+        leftToRightMin[0] = arr[0];
+        for(i=1;i<n;i++)
+        {
+            leftToRightMin[i] = min(leftToRightMin[i-1],arr[i]);
+        }
+        
+        RightToLeftMax[n-1] = arr[n-1];
+        for(i=n-2;i>=0;i--)
+        {
+            RightToLeftMax[i] = max(RightToLeftMax[i+1],arr[i]);
+        }
+        
+        i=0;
+        j=0;
+        
+        while(i<n && j<n)
+        {
+            if(leftToRightMin[i]<=RightToLeftMax[j])
+            {
+                ans = max(ans,j-i);
+                j++;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        return ans;
     }
 };
