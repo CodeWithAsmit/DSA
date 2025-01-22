ebc0952900e2fc826b367086da6035407e9f430f
Asmit Mohan <mohanasmit@gmail.com>
2025-01-23 00:22:46 +0530

    Update Two Sum.cpp

--- 2 Pointer/Two Sum.cpp
+++ 2 Pointer/Two Sum.cpp
@@ -5,46 +5,34 @@ class Solution
 public:
     vector<int> twoSum(vector<int>& nums, int target)
     {
-          vector<int>ans;
-          if(nums.size()==2)
-          {
-               if(nums[0]+nums[1]==target)
-               {
-                   ans.push_back(0);
-                   ans.push_back(1);
-               }
-               return ans;
-          }
+        int low=0;
+        int high=nums.size()-1;   
+        vector<pair<int,int>>vec;
         
-          vector<pair<int,int>>v;
-          for(int i=0;i<nums.size();i++)
-          {
-              v.push_back({nums[i],i});
-          }
-        
-          sort(v.begin(),v.end());
-          int l=0;
-          int r=nums.size()-1;
-          
-          while(l<r)
-          {
-              int sum=v[l].first+v[r].first;
-              
-              if(sum==target)
-              {
-                  ans.push_back(v[l].second);
-                  ans.push_back(v[r].second);
-                  break;
-              }
-              else if(sum>target)
-              {
-                  r--;
-              }
-              else
-              {
-                  l++;
-              }
-          }
-          return ans;
+        for(int i=0;i<nums.size();i++)
+        {
+            vec.push_back({nums[i],i});
+        }
+
+        sort(vec.begin(),vec.end());
+
+        while(low<=high)
+        {
+            long long sum = vec[low].first + vec[high].first;
+
+            if(sum==target)
+            {   
+                return {vec[low].second,vec[high].second};
+            }
+            else if(sum > target)
+            {
+                high--;
+            }
+            else
+            {
+                low++;
+            }
+        }
+        return {};      
     }
 };
