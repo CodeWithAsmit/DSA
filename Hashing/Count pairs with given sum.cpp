85db3026a5e5d0c4f617eb014c5c4f0b4187cc7b
Asmit Mohan <mohanasmit@gmail.com>
2025-04-08 23:00:32 +0530

    Update Count pairs with given sum.cpp

--- Hashing/Count pairs with given sum.cpp
+++ Hashing/Count pairs with given sum.cpp
@@ -19,3 +19,37 @@ public:
         return ans;
     }
 };
+
+/* New question updated code - Time :- O(nlogn) Space :- O(n) */
+
+class Solution
+{
+  public:
+    vector<vector<int>> getPairs(vector<int>& arr)
+    {
+        set<vector<int>>tempSet;
+        unordered_map<int,int>mp;
+        
+        for(int i=0;i<arr.size();i++)
+        {
+            int temp = -arr[i];
+            
+            if(mp.find(temp)!=mp.end())
+            {
+                if(temp>0)
+                {
+                    temp = -temp;
+                }
+                tempSet.insert({temp,-temp});
+            }
+            else
+            {
+                if(mp.find(arr[i])==mp.end())
+                {
+                    mp[arr[i]] = i;
+                }
+            }
+        }
+        return vector<vector<int>>(tempSet.begin(), tempSet.end());
+    }
+};
