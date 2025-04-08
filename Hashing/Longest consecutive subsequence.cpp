012887226e4e55c514e7c926945400f289398530
Asmit Mohan <mohanasmit@gmail.com>
2025-04-09 00:51:46 +0530

    Update Longest consecutive subsequence.cpp

--- Hashing/Longest consecutive subsequence.cpp
+++ Hashing/Longest consecutive subsequence.cpp
@@ -1,74 +1,103 @@
-/* Approach 1  --> Efficient Approach Time :- O(N) Space :- O(N) */
+/* Approach 1 :- Brute Force */
 
 class Solution
 {
 public:
     int longestConsecutive(vector<int>& nums)
     {
-        int ans=INT_MIN;
-        unordered_map<int,int>mp;
+        int ans = 0;
+        unordered_map<int,int>mp1;
+        unordered_map<int,int>mp2;
 
-        for(auto x : nums)
+        for(int i = 0; i < nums.size(); i++)
         {
-            mp[x]++;
+            mp1[nums[i]] = i;
         }
-        for(int i=0;i<nums.size();i++)
-        {
-            int count=0;
-            int temp=nums[i];
-            if(mp.find(temp-1)==mp.end())
-            {
-                while(mp.find(temp)!=mp.end())
-                {
-                    temp++;
-                    count++;
-                }
-                ans=max(ans,count);
-            }
-        }
-        return ans==INT_MIN?0:ans;          
-    }
-};
 
-/* Approach 2 Less Efficient Time :- O(Nlogn) Space :- O(1) */
+        for(auto it : mp1)
+        {
+            int num = it.first;
 
-class Solution
-{
-public:
-    int longestConsecutive(vector<int>& nums)
-    {
-        int n=nums.size();
-        if(n==0)
-        {
-            return 0;
-        }
-        if(n==1)
-        {
-            return 1;
-        }
-        sort(nums.begin(),nums.end());
-        long long count=1;
-        long long ans=0;
-        for(int i=1;i<n;i++)
-        {
-            if(nums[i]==nums[i-1])
+            if(mp2.find(num) != mp2.end())
             {
                 continue;
             }
-            if((nums[i]-nums[i-1])==1)
+
+            int temp = num;
+            int count = 1;
+
+            while(mp1.find(temp - 1) != mp1.end())
             {
+                temp--;
+                if(mp2.find(temp) != mp2.end())
+                {
+                    count += mp2[temp];
+                    break;
+                }
                 count++;
-                ans=max(ans,count);
             }
-            else
-            {
-                count=1;
-            }
+
+            mp2[num] = count;
+            ans = max(ans, count);
         }
-        if(ans==0)
+        return ans;
+    }
+};
+
+/* Approach 2 :- Most Optimal Approach --> By memorising more efficiently and productive for next number */
+
+class Solution
+{
+public:
+    int longestConsecutive(vector<int>& nums)
+    {
+        if (nums.empty())
         {
-            return 1;
+            return 0;
         }
-        return ans;    
+
+        int ans = 0;
+        unordered_map<int, int> mp1; 
+        unordered_map<int, int> mp2;
+        
+        for(int i = 0; i < nums.size(); i++)
+        {
+            mp1[nums[i]] = i;
+        }
+
+        for (auto it : mp1)
+        {
+            int num = it.first;
+
+            if (mp2.find(num) != mp2.end()) 
+            {
+                continue;
+            }
+
+            int curr = num;
+            int count = 0;
+
+            vector<int> memorise;
+
+            while (mp1.find(curr) != mp1.end() && mp2.find(curr) == mp2.end())
+            {
+                memorise.push_back(curr);
+                curr--;
+                count++;
+            }
+
+            if (mp2.find(curr) != mp2.end())
+            {
+                count += mp2[curr];
+            }
+
+            for (int i = 0; i < memorise.size(); ++i)
+            {
+                mp2[memorise[i]] = count - i;
+            }
+            
+            ans = max(ans, count);
+        }
+        return ans;
     }
 };
