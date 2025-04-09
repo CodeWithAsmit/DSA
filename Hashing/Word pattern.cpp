67462f3785716fc8878a3f78e55c210bcb39fe0a
Asmit Mohan <mohanasmit@gmail.com>
2025-04-10 00:30:59 +0530

    Update Word pattern.cpp

--- Hashing/Word pattern.cpp
+++ Hashing/Word pattern.cpp
@@ -3,48 +3,56 @@
 class Solution
 {
 public:
-    unordered_map<char,string>mp1;
-    unordered_map<string,char>mp2;
-
-    bool solve(char c,string word)   /* This is taking O(1) time */
+    bool wordPattern(string pattern, string s)
     {
-        if(mp1.find(c)!=mp1.end()&&mp1[c]!=word)
+        string temp = "";
+        vector<string>words;
+
+        unordered_map<char,string>mp1;
+        unordered_map<string,char>mp2;
+        
+        if(pattern.length() == 1 && s.length() == 1)
+        {
+            return 1;
+        }
+
+        for(auto it : s)
+        {
+            if(it != ' ')
+            {
+                temp.push_back(it);
+            }
+            else
+            {
+                words.push_back(temp);
+                temp="";
+            }
+        }
+
+        if(temp.size() > 0)
+        {
+            words.push_back(temp);
+        }
+
+        if(pattern.length() != words.size())
         {
             return 0;
         }
-        else if(mp2.find(word)!=mp2.end()&&mp2[word]!=c)
+
+        for(int i=0;i<pattern.size();i++)
         {
-            return 0;
-        }
-        else if(mp1.find(c)==mp1.end())
-        {
-            mp1[c]=word;
-            mp2[word]=c;
+           bool possibility1 = mp1.find(pattern[i]) != mp1.end() && mp1[pattern[i]] != words[i];
+
+           bool possibility2 = mp2.find(words[i]) != mp2.end() && mp2[words[i]] != pattern[i];
+
+           if(possibility1 || possibility2)
+           {
+                return 0;
+           }
+
+           mp1[pattern[i]] = words[i];
+           mp2[words[i]] = pattern[i];
         }
         return 1;
-    }
-
-    bool wordPattern(string pat, string s)
-    {
-        vector<string>dict;
-        stringstream str(s);
-        string word;
-        while (str >> word)
-        {
-            dict.push_back(word);
-        }
-
-        if(pat.length()!=dict.size())
-        {
-            return 0;
-        }
-        for(int i=0;i<pat.length();i++)
-        {
-            if(!solve(pat[i],dict[i]))
-            {
-                return 0;
-            }          
-        }
-        return 1;      
     }
 };
