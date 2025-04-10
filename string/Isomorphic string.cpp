360cf9ef3ffdec76679edcd6fa5c751cedc3e1e3
Asmit Mohan <mohanasmit@gmail.com>
2025-04-10 22:36:20 +0530

    Update Isomorphic string.cpp

--- string/Isomorphic string.cpp
+++ string/Isomorphic string.cpp
@@ -5,33 +5,26 @@ class Solution
 public:
     bool isIsomorphic(string s, string t)
     {
-       if(s.length()!=t.length())
-       {
-           return 0;
-       }
-       unordered_map<char,char>mp1,mp2;
-       for(int i=0;i<s.length();i++)
-       {
-           if(mp1.find(s[i])!=mp1.end())
-           {
-               if(mp1[s[i]]!=t[i])
-               {
-                   return 0;
-               }
-           }
-           else if(mp2.find(t[i])!=mp2.end())
-           {
-               if(mp2[t[i]]!=s[i])
-               {
-                   return 0;
-               }
-           }
-           else
-           {
-               mp1[s[i]]=t[i];
-               mp2[t[i]]=s[i];
-           }
-       }
-       return 1;
+        unordered_map<char,char>map1;
+        unordered_map<char,char>map2;
+
+        if (s.length() != t.length())
+        {
+            return 0;
+        }         
+
+        for(int i=0;i<s.length();i++)
+        {
+            bool possiblity1 = map1.find(s[i]) != map1.end() && map1[s[i]] != t[i];
+            bool possiblity2 = map2.find(t[i]) != map2.end() && map2[t[i]] != s[i]; 
+
+            if(possiblity1 || possiblity2)
+            {
+                return 0;
+            }      
+            map1[s[i]] = t[i];
+            map2[t[i]] = s[i];
+        }
+        return 1;
     }
 };
