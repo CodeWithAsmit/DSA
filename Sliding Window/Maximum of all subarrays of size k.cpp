0f954a63fa2690ef4213f57fddc9597b4dcf2cb1
Asmit Mohan <mohanasmit@gmail.com>
2025-04-13 11:25:37 +0530

    Update Maximum of all subarrays of size k.cpp

--- Sliding Window/Maximum of all subarrays of size k.cpp
+++ Sliding Window/Maximum of all subarrays of size k.cpp
@@ -1,4 +1,65 @@
-/* Time :- O(N) Space :- O(K) */
+/* Approach 1 :- Time :- O(N*K) Space :- O(K) */
+
+class Solution
+{
+  public:
+  
+    int findMaxValue(unordered_map<int,int> &map)
+    {
+        int maxValue = INT_MIN;
+        
+        for(auto it : map)
+        {
+            maxValue = max(maxValue,it.second);
+        }
+        
+        return maxValue;
+    }
+    
+    vector<int> maxOfSubarrays(vector<int>& arr, int k)
+    {
+        if(k==1)
+        {
+            return arr;
+        }
+        
+        int i=0;
+        int j=0;
+        int maxValue = INT_MIN;
+        
+        vector<int>ans;
+        unordered_map<int,int>map;
+        
+        while(j<arr.size())
+        {
+            if(j-i+1 == k)
+            {
+                map.erase(i);
+                map[j] = arr[j];
+                
+                maxValue = max(maxValue,arr[j]);
+                ans.push_back(maxValue);
+                
+                if(arr[i] == maxValue)
+                {
+                    maxValue = findMaxValue(map);
+                }
+                
+                i++;
+                j++;
+            }
+            else
+            {
+                map[j] = arr[j];
+                maxValue = max(maxValue,arr[j]);
+                j++;
+            }
+        }
+        return ans;
+    }
+};
+
+/* Approach 2 :- Time :- O(N) Space :- O(K) */
 
 class Solution
 {
