ddc5982db3299b9c91fbcf385d2d056f50619ed6
Asmit Mohan <mohanasmit@gmail.com>
2025-04-13 11:50:24 +0530

    Update Max Sum Subarray of size K .cpp

--- Sliding Window/Max Sum Subarray of size K .cpp
+++ Sliding Window/Max Sum Subarray of size K .cpp
@@ -1,29 +1,31 @@
 /* Time :- O(N) Space :- O(1) */
 
 class Solution
-{   
-public:
-    int maximumSumSubarray(int K,vector<int> &Arr,int N)
+{
+  public:
+    int maximumSumSubarray(vector<int>& arr, int k)
     {
-         int i=0;
-         int j=0;
-         int mx=INT_MIN;
-         int sum=0;
-         while(j<N)
-         {
-             sum=sum+Arr[j];
-             if(j-i+1==K)
-             {
-                 mx=max(mx,sum);
-                 sum=sum-Arr[i];
-                 i++;
-                 j++;
-             }
-             else
-             {
-                 j++;
-             }
-         }
-         return mx ;
+        int i=0;
+        int j=0;
+        int sum = 0;
+        int ans = INT_MIN;
+        
+        while(j<arr.size())
+        {
+            sum = sum  + arr[j];
+            
+            if(j-i+1 == k)
+            {
+                ans = max(ans,sum);
+                sum = sum - arr[i];
+                i++;
+                j++;
+            }
+            else
+            {
+                j++;
+            }
+        }
+        return ans;
     }
 };
