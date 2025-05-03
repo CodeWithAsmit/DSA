798607776767e8f483c1c543e94f2ad23da02886
Asmit Mohan <mohanasmit@gmail.com>
2025-05-03 12:47:57 +0530

    Update Longest-substring-without-repeating-characters.cpp

--- Sliding Window/Longest-substring-without-repeating-characters.cpp
+++ Sliding Window/Longest-substring-without-repeating-characters.cpp
@@ -1,4 +1,35 @@
-/* Time :- O(N) Space :- O(N) */
+/* Time :- O(N*N) Space :- O(N) --> Optimised Brute Force */
+
+class Solution
+{
+public:
+    int lengthOfLongestSubstring(string s)
+    {
+        int maxLength = 0;
+        int len = s.length();
+
+        for (int i = 0; i < len; ++i)
+        {
+            unordered_set<char> seenChars;
+            for (int j = i; j < len; ++j)
+            {
+                char currentChar = s[j];
+                if (seenChars.count(currentChar))
+                {
+                    break;
+                }
+                else
+                {
+                    seenChars.insert(currentChar);
+                    maxLength = max(maxLength, j - i + 1);
+                }
+            }
+        }
+        return maxLength;
+    }
+};
+
+/* Time :- O(N) Space :- O(K) */
 
 class Solution
 {
@@ -37,5 +68,35 @@ public:
             }
         }
         return maxi;    
+    }
+};
+
+/* Time :- O(N) Space :- O(K) --> More optimal approach with same time and space as it is not calculating freq. just checking the presence */
+
+class Solution
+{
+public:
+    int lengthOfLongestSubstring(string s)
+    {
+        int i = 0;
+        int j = 0;
+        int maxLength = 0;
+        unordered_set<char> seen;
+
+        while (j < s.length())
+        {
+            if (seen.find(s[j]) == seen.end())
+            {
+                seen.insert(s[j]);
+                maxLength = max(maxLength, j - i + 1);
+                j++;
+            }
+            else
+            {
+                seen.erase(s[i]);
+                i++;
+            }
+        }
+        return maxLength;
     }
 };
