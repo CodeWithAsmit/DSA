ad57ffab4d080fb2a1aaf4b1b5a61a845d5082a9
Asmit Mohan <mohanasmit@gmail.com>
2025-05-04 22:06:44 +0530

    Update First and Last occurences of x.cpp

--- Searching/First and Last occurences of x.cpp
+++ Searching/First and Last occurences of x.cpp
@@ -1,4 +1,63 @@
-/* Time :- O(logn) Space :- O(1) */
+/* Approach 1 :- Time :- O(logn) [Best and Average] O(N) [Worst Case] Space :- O(1) */
+
+class Solution
+{
+    public:
+    int search(vector<int>& nums, int target)
+    {
+        int low = 0;
+        int high = nums.size()-1;
+
+        while(low<=high)
+        {
+            int mid = high + (low-high)/2;
+
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            else if(nums[mid]>target)
+            {
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+        return -1;
+    }
+    vector<int> searchRange(vector<int>& arr, int target)
+    {
+        if(arr.empty())
+        {
+            return {-1, -1};
+        }
+
+        int pos = search(arr, target);
+
+        if(pos == -1)
+        {
+            return {-1, -1};
+        }
+
+        int init = pos;
+        int last = pos;
+
+        while(init - 1 >= 0 && arr[init - 1] == target)
+        {
+            init--;
+        }
+
+        while(last + 1 < arr.size() && arr[last + 1] == target)
+        {
+            last++;
+        }
+        return {init, last};
+    }
+};
+
+/* Approach 2 :- Time :- O(logn) Space :- O(1) */
 
 class Solution
 {
