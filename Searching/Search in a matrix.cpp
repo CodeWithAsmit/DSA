c6f7f99f4059f64bf12a599af69af2353000bcd0
Asmit Mohan <mohanasmit@gmail.com>
2025-05-04 22:45:13 +0530

    Update Search in a matrix.cpp

--- Searching/Search in a matrix.cpp
+++ Searching/Search in a matrix.cpp
@@ -1,27 +1,30 @@
-/* Time :- O(n+m) Space :- O(1) */
+/* Time :- O(M + N) Space :- O(1) */
 
 class Solution
 {
-public:	
-	int matSearch (vector<vector<int>> &arr, int N, int M, int X)
-	{
-	   int row=0;
-	   int col=M-1;
-	   while(row<N&&col>-1)
-	   {
-	       if(arr[row][col]==X)
-	       {
-	           return 1;
-	       }
-	       else if(arr[row][col]>X)
-	       {
-	           col--;
-	       }
-	       else
-	       {
-	           row++;
-	       }
-	   }
-	   return 0;
-       }
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target)
+    {
+        int rowCount = matrix.size();
+        int colCount = matrix[0].size();
+        int low = 0;
+        int high = colCount - 1;
+        
+        while(low<rowCount && high>=0)
+        {
+            if(matrix[low][high]==target)
+            {
+                return 1;
+            }
+            else if(matrix[low][high] > target)
+            {
+                high--;
+            }
+            else
+            {
+                low++;
+            }
+        }
+        return 0;
+    }
 };
