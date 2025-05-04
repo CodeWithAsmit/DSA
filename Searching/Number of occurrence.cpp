395cbf8628d473ceed3e5d9bfcaadd0b2f2e46be
Asmit Mohan <mohanasmit@gmail.com>
2025-05-04 14:40:46 +0530

    Update Number of occurrence.cpp

--- Searching/Number of occurrence.cpp
+++ Searching/Number of occurrence.cpp
@@ -1,8 +1,62 @@
 /* Approach 1 --> Using Hashmap to count the frequency --> Time :- O(N) Space :- O(N) */
 
-/* Approach 2 --> Using Binary Search Time :- O(logn) Space :- O(1) */
+/* Approach 2 --> Using Partially Binary Search Time :- O(N) Space :- O(1) */
 
-/* Time :- O(logn) Space :- O(1) */
+class Solution
+{
+    public:
+    int search(vector<int>& nums, int target)
+    {
+        int low = 0;
+        int high = nums.size()-1;
+
+        while(low<=high)
+        {
+            int mid = high + (low-high)/2;
+
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            else if(nums[mid]>target)
+            {
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+        return -1;
+    }
+    
+    int countFreq(vector<int>& arr, int target)
+    {
+        int pos = search(arr,target);
+        
+        if(pos==-1)
+        {
+            return 0;
+        }
+        
+        int init = pos;
+        int last = pos;
+        
+        while(arr[init]==target)
+        {
+            init--;
+        }
+        
+        while(arr[last]==target)
+        {
+            last++;
+        }
+        
+        return last-init-1;
+    }
+};
+
+/* Approach 3 --> Using Binary Search Time :- O(logn) Space :- O(1) */
 
 class Solution
 {
