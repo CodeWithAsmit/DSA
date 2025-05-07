4b8a25a47f0912b782e2864de59d9d419055c28f
Asmit Mohan <mohanasmit@gmail.com>
2025-05-08 00:01:13 +0530

    Update Peak element.cpp

--- Searching/Peak element.cpp
+++ Searching/Peak element.cpp
@@ -3,26 +3,29 @@
 class Solution
 {
 public:
-    int peakIndexInMountainArray(vector<int>& nums)
+    int findPeakElement(vector<int>& nums)
     {
         int low = 0;
-        int high = nums.size()-1;
+        int high = nums.size() - 1;
 
-        while(low<=high)
+        while(low <= high)
         {
-            int mid = high + (low-high)/2;
+            int mid = low + (high - low) / 2;
 
-            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1])
+            bool leftOk = (mid == 0) || (nums[mid] > nums[mid - 1]);
+            bool rightOk = (mid == nums.size() - 1) || (nums[mid] > nums[mid + 1]);
+
+            if(leftOk && rightOk)
             {
                 return mid;
             }
-            else if(nums[mid]>nums[mid+1])
+            else if(mid > 0 && nums[mid - 1] > nums[mid])
             {
-                high=mid-1;
+                high = mid - 1;
             }
             else
             {
-                low=mid+1;
+                low = mid + 1;
             }
         }
         return -1;
