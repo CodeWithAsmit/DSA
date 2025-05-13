ebf689742d6ebc57fc339ca6f1eb4f7ea8e2bac7
Asmit Mohan <mohanasmit@gmail.com>
2025-05-13 20:50:52 +0530

    Update Nth root of a number.cpp

--- Searching/Nth root of a number.cpp
+++ Searching/Nth root of a number.cpp
@@ -2,52 +2,43 @@
 
 class Solution
 {
-	public:
-	int solve(int n,int x,int target)  /* O(N) */
-	{
-	   unsigned long long ans=1;
-	   while(n--)
-	   {
-	       ans=ans*x;
-	       if(ans>target)
-	       {
-	           return -1;
-	       }
-	   }
-	   return (int)ans;
-	}
-	
-	int NthRoot(int n, int m)
-	{
-	    if(n==1)
-	    {
-	        return m;
-	    }
-	    if(m==1)
-	    {
-	        return 1;
-	    }
-	    
-            int low=0;
-            int high=m/2;
-        
-            while(low<=high)             /* O(logm) */
+  public:
+    long long solve(int mid,int number,int target)
+    {
+        long long temp=1;
+        while(number--)
+        {
+            temp = mid * temp;
+            if(temp > target)
             {
-		 int mid=low+(high-low)/2;
-		 int ans=solve(n,mid,m);
-		 if(ans==m)
-		 {
-		     return mid;
-		 }
-		 else if(ans==-1)
-		 {
-		     high=mid-1;
-		 }
-		 else
-		 {
-	             low=mid+1;
-		 }
+                break;
             }
-       return -1;
-    }  
+        }
+        return temp;
+    }
+    int nthRoot(int n, int m)
+    {
+        int low = 1;
+        int high= m;
+
+        while(low<=high)
+        {
+            int mid = low + (high-low)/2;
+            long long product = solve(mid,n,m);
+            
+            if(product==m)
+            {
+                return mid;
+            }
+            else if(product > m)
+            {
+                high = mid-1;
+            }
+            else
+            {
+                low = mid+1;
+            }
+        }
+        return -1;  
+    }
 };
