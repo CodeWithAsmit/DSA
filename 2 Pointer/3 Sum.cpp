baf809e7847e2b7d34435206de835f86e019667d
Asmit Mohan <mohanasmit@gmail.com>
2025-06-15 19:33:30 +0530

    Update 3 Sum.cpp

--- 2 Pointer/3 Sum.cpp
+++ 2 Pointer/3 Sum.cpp
@@ -1,115 +1,112 @@
 /* Time :- O(N*N) Space :- O(N) */
 
 /* Approach 1 :- very least efficient */
-class Solution
-{
-public:
-vector<vector<int>> threeSum(vector<int>& nums)
-{
-        set<vector<int>>ans;
-        int n=nums.size();
-        sort(nums.begin(), nums.end());
-        for (int i = 0; i < n-1; i++)
-        {
-            int l = i+1;
-            int r = n-1;
-            while (l < r)
-            {
-                if (nums[i] + nums[l] + nums[r] == 0)
-                {
-                    vector<int>v{nums[i], nums[l], nums[r]};
-                    ans.insert(v);
-                    l++;
-                    r--;
-                }
-                else if (nums[i] + nums[l] + nums[r] < 0)
-                {
-                    l++;
-                }
-                else
-                {
-                    r--;
-                }
-            }
-        }
-        vector<vector<int>>res(ans.begin(), ans.end());
-        return res;
-}
-};
 
-/*Approch 2:- Efficient*/
-class Solution
-{
-public:
-vector<vector<int>> threeSum(vector<int>& nums)
-{
-vector<vector<int>>res;
-if(nums.size()<3)
-{
-    return res;
-}
-    
-sort(nums.begin(),nums.end());    
-for(int i=0;i<nums.size()-2;++i)
-{
-    int l=i+1;
-    int r=nums.size()-1;
-    if(i>0 and nums[i]==nums[i-1])
-    {
-        continue;
-    }
-    while(l<r)
-    {
-      int sum=nums[i]+nums[l]+nums[r];
-      if(sum==0)
-         {
-             vector<int>let;
-             let.push_back(nums[i]);
-             let.push_back(nums[l]);
-             let.push_back(nums[r]);
-          
-             int s=res.size();      
-             res.push_back(let);
-             l++;
-             r--;
-          
-             while(l<nums.size() and nums[l-1]==nums[l])
-             { 
-                 l++;
-             }
-             while(r>0 and nums[r]==nums[r+1] )
-             {
-                 r--;
-             }           
-        }
-        else if(sum>0)
-        {        
-           r--;
-        }
-        else if(sum<0)
-        {            
-            l++;
-        }                   
-    }  
-}
-return res;
-}
-};
-
-/*Approach 3 :- Most Efficient approach*/
 class Solution
 {
 public:
     vector<vector<int>> threeSum(vector<int>& nums)
     {
-        vector<vector<int>>v;
-        if(nums.size()<3)
+        vector<vector<int>>ans;
+        set<vector<int>>filterVector;
+        sort(nums.begin(),nums.end());
+
+        for(int i=0;i<nums.size();i++)
         {
-            return v;
+            int low = i+1;
+            int high = nums.size()-1;
+
+            while(low<high)
+            {
+                int sum = nums[i] + nums[low] + nums[high];
+                if(sum==0)
+                {
+                    filterVector.insert({nums[i], nums[low], nums[high]});
+                    low++;
+                    high--;
+                }
+                else if(sum>0)
+                {
+                    high--;
+                }
+                else
+                {
+                    low++;
+                }
+            }
         }
+        for(auto it : filterVector)
+        {
+            ans.push_back(it);
+        }
+        return ans;
+    }
+};
+
+/* Approch 2:- Efficient */
+
+class Solution
+{
+public:
+    vector<vector<int>> threeSum(vector<int>& nums)
+    {
+        vector<vector<int>> res;    
+        sort(nums.begin(), nums.end());  
+
+        for (int i = 0; i < nums.size() - 2; ++i)
+        {
+            if (i > 0 && nums[i] == nums[i - 1])
+            {
+                continue;
+            }
+
+            int low = i + 1;
+            int high = nums.size() - 1;
+
+            while (low < high)
+            {
+                int sum = nums[i] + nums[low] + nums[high];
+                if (sum == 0)
+                {
+                    res.push_back({nums[i], nums[low], nums[high]});
+                    low++;
+                    high--;
+
+                    while (low < high && nums[low] == nums[low - 1])
+                    {
+                        low++;
+                    }
+                    while (low < high && nums[high] == nums[high + 1])
+                    {
+                        high--;
+                    }
+                }
+                else if (sum > 0)
+                {
+                    high--;
+                }
+                else
+                {
+                    low++;
+                }                   
+            }  
+        }
+        return res;
+    }
+};
+
+/*Approach 3 :- Most Efficient approach*/
+
+class Solution
+{
+public:
+    vector<vector<int>> threeSum(vector<int>& nums)
+    {
+        int n = nums.size();  
+        vector<vector<int>>v;
         set<vector<int>>ans;
-        sort(nums.begin(), nums.end());
-        int n = nums.size();    
+        sort(nums.begin(), nums.end());  
+            
         for(int i = 0; i < n-2; i++)
         {
             if(i>0&&nums[i]==nums[i-1])
@@ -135,7 +132,6 @@ public:
                 }
             }
         }
-    
         for(auto i : ans)
         {
             v.push_back(i);
