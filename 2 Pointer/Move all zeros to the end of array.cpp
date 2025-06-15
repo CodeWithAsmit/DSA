44d90ba97fdc23a790a533fb0142b2fffbeb2551
Asmit Mohan <mohanasmit@gmail.com>
2025-06-15 21:23:11 +0530

    Update Move all zeros to the end of array.cpp

--- 2 Pointer/Move all zeros to the end of array.cpp
+++ 2 Pointer/Move all zeros to the end of array.cpp
@@ -1,3 +1,29 @@
+/* Approach 1 */
+
+class Solution
+{
+    public:
+    void pushZerosToEnd(vector<int>& arr)
+    {
+        int nonZeroIndex = 0;
+        
+        for(int i=0;i<arr.size();i++)
+        {
+            if(arr[i]!=0)
+            {
+                arr[nonZeroIndex++] = arr[i];
+            }
+        }
+        
+        while(nonZeroIndex < arr.size())
+        {
+            arr[nonZeroIndex++] = 0;
+        }
+    }
+};
+
+/* Approach 2 */
+
 class Solution
 {
 public:
