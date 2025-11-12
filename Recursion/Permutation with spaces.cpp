5c4fce4718cf255f74bf89d35ed662610eed4bec
Asmit Mohan <mohanasmit@gmail.com>
2025-11-12 23:34:47 +0530

    Update Permutation with spaces.cpp

--- Recursion/Permutation with spaces.cpp
+++ Recursion/Permutation with spaces.cpp
@@ -4,36 +4,66 @@
 
 class Solution
 {
-public:
-    void solve(string s,string op,vector<string>&ans)
+    public:
+    void solve(int currLength, int totalLength, string tempString, string mainString, vector<string> &ans)
     {
-        if(s.length()==0)
+        if(currLength==totalLength)
         {
-           ans.push_back(op);
-           return;
+            ans.push_back(tempString);
+            return;
         }
         
-        string op1=op;
-        string op2=op;
+        string tempAddChar = tempString + mainString[0];
+        string tempAddSpace = tempString + ' ' + mainString[0];
+        mainString.erase(0,1);
         
-        op1.push_back(s[0]);
-        op2.push_back(' ');
-        op2.push_back(s[0]);
-        
-        s.erase(s.begin()+0);
-        
-        solve(s,op1,ans);
-        solve(s,op2,ans);
+        solve(currLength + 1, totalLength, tempAddSpace, mainString, ans);
+        solve(currLength + 1, totalLength, tempAddChar, mainString, ans);
+        return;
     }
     
-    vector<string> permutation(string S)
+    vector<string> permutation(string mainString)
     {
-        vector<string>ans;
-        string op="";
-        op.push_back(S[0]);
-        S.erase(S.begin()+0);
-        solve(S,op,ans);
-        sort(ans.begin(),ans.end());
-        return ans;
+        string tempString;
+        vector<string>result;
+        
+        int length = mainString.length();   
+        tempString.push_back(mainString[0]);
+        
+        mainString.erase(0,1);
+        solve(1,length,tempString,mainString,result);
+        return result;
+    }
+};
+
+/* More optimal by removing erase function --> But still time and space is same as above */
+
+class Solution
+{
+public:
+    void solve(int index, int n, string tempString, const string &mainString, vector<string> &ans)
+    {
+        if (index == n)
+        {
+            ans.push_back(tempString);
+            return;
+        }
+        
+        solve(index + 1, n, tempString + " " + mainString[index], mainString, ans);
+        solve(index + 1, n, tempString + mainString[index], mainString, ans);
+    }
+
+    vector<string> permutation(string mainString)
+    {
+        vector<string> result;
+        
+        if (mainString.empty())
+        {
+            return ans;
+        }
+        
+        string tempString(1, mainString[0]);
+        solve(1, mainString.size(), tempString, mainString, result);
+        return result;
     }
 };
