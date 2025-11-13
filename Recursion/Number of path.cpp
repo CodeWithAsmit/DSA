576f97c37febeb6570ebef0b89a8a7d0daf17dd6
Asmit Mohan <mohanasmit@gmail.com>
2025-11-13 09:58:52 +0530

    Refactor number of paths calculation using classes

--- Recursion/Number of path.cpp
+++ Recursion/Number of path.cpp
@@ -1,13 +1,66 @@
-/* Approach 1 --> Recursion Time :- O(2^n) Space :- O(1) */
+/* Approach 1 --> Recursion Time :- O() Space :- O() */
 
-Long Long  numberOfPaths(int m, int n)
+class Solution
 {
-    if(m==1||n==1)
+    public:
+    int solve(int curRow, int curCol, int totalRow, int totalCol)
     {
-        return 1;
+        if(curRow<0 || curCol<0)
+        {
+            return 0;
+        }
+        
+        if(curRow==0 || curCol==0)
+        {
+            return 1;
+        }
+        
+        int wayUp = solve(curRow-1, curCol, totalRow, totalCol);
+        int wayLeft = solve(curRow, curCol-1, totalRow, totalCol);
+        return wayUp + wayLeft;
     }
-    return numberOfPaths(m-1,n)+numberOfPaths(m,n-1);
-}
+    
+    int numberOfPaths(int m, int n)
+    {
+        return solve(m-1,n-1,m,n);
+    }
+};
+
+/* Approach 2 --> Recursion Memorized :- O() Space :- O() */
+
+class Solution
+{
+    public:
+    int pathMemo[101][101];
+    int solve(int curRow, int curCol, int totalRow, int totalCol)
+    {
+        if(curRow<0 || curCol<0)
+        {
+            return 0;
+        }
+        
+        if(curRow==0 || curCol==0)
+        {
+            return 1;
+        }
+        
+        if(pathMemo[curRow][curCol]!=-1)
+        {
+            return pathMemo[curRow][curCol];
+        }
+        
+        int wayUp = solve(curRow-1, curCol, totalRow, totalCol);
+        int wayLeft = solve(curRow, curCol-1, totalRow, totalCol);
+        return pathMemo[curRow][curCol] = wayUp + wayLeft;
+    }
+    
+    int numberOfPaths(int m, int n)
+    {
+        memset(pathMemo, -1, sizeof(pathMemo));
+        return solve(m-1,n-1,m,n);
+    }
+};
+
 
 /* Efficient Approach 2 --> DP Time :- O(m*n) Space :- O(m*n) */
 
