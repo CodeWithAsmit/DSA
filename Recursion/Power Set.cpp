b5710434838db6e0b7e20617d36965cf10c0bcff
Asmit Mohan <mohanasmit@gmail.com>
2025-11-15 00:21:05 +0530

    Refactor power set generation method
    Refactor the solve function to use indices instead of string manipulation for generating power set.

--- Recursion/Power Set.cpp
+++ Recursion/Power Set.cpp
@@ -1,37 +1,27 @@
 /* Time :- O[n*(2^n)] Space :- O[n*(2^n)] */
 
-/* Solve function time complexity is O(N) beacuse of erase function and solve function is called O(2^n) times so overall time is --> O[n*(2^n)] */
-
-class Solution
+class Solution 
 {
-	public:
-	void solve(string s,string op,vector<string>&ans)
-	{
-	    if(s.length()==0)
-	    {
-	        if(op!="")
-	        {
-	            ans.push_back(op); /*Not Required Empty String*/
-	        }
-	        return;
-	    }
-		
-	    string op1=op;
-	    string op2=op;
-		
-	    op2.push_back(s[0]);
-	    s.erase(s.begin()+0);
-		
-	    solve(s,op1,ans);
-	    solve(s,op2,ans);
-	    return;
-	}
-	vector<string> AllPossibleStrings(string s)
-	{
-		string op="";
-		vector<string>ans;
-		solve(s,op,ans);
-		sort(ans.begin(),ans.end());
-		return ans;
-	}
+    public:
+    void solve(int curLength, string &tempString, string &mainString, vector<string>&result)
+    {
+        if(curLength==mainString.length())
+        {
+            result.push_back(tempString);
+            return;
+        }
+        
+        string newString = tempString + mainString[curLength];
+        solve(curLength+1, tempString, mainString, result);
+        solve(curLength+1, newString, mainString, result);
+        return;
+    }
+    vector<string> AllPossibleStrings(string mainString)
+    {
+        string tempString="";
+        vector<string>result;
+        solve(0,tempString,mainString,result);
+        sort(result.begin(),result.end());
+        return result;
+    }
 };
